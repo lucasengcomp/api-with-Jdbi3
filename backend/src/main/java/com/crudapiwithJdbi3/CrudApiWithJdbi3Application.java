11f45af70c7890f7114d7b3efc480f5f8258c6d7
Lucas Galvao <lucas.engcomp@outlook.com>
2022-09-05 19:27:21 -0300

    Project created

---
+++ backend/src/main/java/com/crudapiwithJdbi3/CrudApiWithJdbi3Application.java
@@ -0,0 +1,13 @@
+package com.crudapiwithJdbi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiWithJdbi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiWithJdbi3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/crudapiwithJdbi3/CrudApiWithJdbi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudapiwithJdbi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiWithJdbi3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
